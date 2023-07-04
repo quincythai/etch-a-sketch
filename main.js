4857eb64fb5a1cbca816e9066d9c1ee87345e466
Quincy <quincythai50@icloud.com>
2023-07-03 23:04:10 -0700

    Implement grid and changeBoxColor function

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
-// Create webpage of 16x16 grid of square divs
-let board = document.getElementById("board");
+const grid = document.getElementById('grid');
+let color = "";
 
-function createGrid(numRows, numCols) {
-    for (let i = 0; i < numRows; i++) {
-        let row = document.createElement('div');
-        row.className = "grid-row";
-        board.appendChild(row);
+function createGrid(size) {
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`; // 1fr - 1 fraction of the row's size
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-        for (let j = 0; j < numCols; j++) {
-            let box = document.createElement('div');
-            box.className = "box";
-            row.appendChild(box);
-        }
+    for (let i = 0; i < size * size; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box'); // add box styling
+        box.addEventListener('mouseover', changeBoxColor);
+        grid.appendChild(box);
     }
 }
 
-createGrid(16, 16);
+createGrid(16);
 
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach(box => {
-    color = "Black";
-    box.addEventListener('mouseover', console.log("box"));
-});
-
-function changeBoxColor(color) {
-    this.style.backgroundColor = color;
+function changeBoxColor(e) {
+    /* Event object itself doesn't have backgroundColor property 
+    So we need e.target.style... instead of just e.style...*/
+    e.target.style.backgroundColor = "black";
 }
